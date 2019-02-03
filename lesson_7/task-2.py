afe0e778d37a0f1dfa8385c214f35d9487252507
kirill <kirill.rum@gmail.com>
2019-02-03 12:44:00 +0300

    +

--- lesson_7/task-2.py
+++ lesson_7/task-2.py
@@ -1,2 +1,46 @@
 # 2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
 # заданный случайными числами на промежутке [0; 50). Выведите на экран исходный и отсортированный массивы.
+
+import random
+
+
+list = []
+
+for i in range(44):
+    list.append(random.randint(0, 50))
+print(list)
+
+
+def mergeSort(alist):
+    if len(alist) > 1:
+        mid = len(alist) // 2
+        lefthalf = alist[:mid]
+        righthalf = alist[mid:]
+
+        mergeSort(lefthalf)
+        mergeSort(righthalf)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(lefthalf) and j < len(righthalf):
+            if lefthalf[i] < righthalf[j]:
+                alist[k] = lefthalf[i]
+                i = i + 1
+            else:
+                alist[k] = righthalf[j]
+                j = j + 1
+            k = k + 1
+
+        while i < len(lefthalf):
+            alist[k] = lefthalf[i]
+            i = i + 1
+            k = k + 1
+
+        while j < len(righthalf):
+            alist[k] = righthalf[j]
+            j = j + 1
+            k = k + 1
+
+mergeSort(list)
+print(list)
--- lesson_7/task-3.py
+++ lesson_7/task-3.py
@@ -2,3 +2,23 @@
 # Медианой называется элемент ряда, делящий его на две равные части: в одной находятся элементы, которые не меньше медианы, в другой – не больше медианы.
 # Задачу можно решить без сортировки исходного массива. Но если это слишком сложно, то используйте метод сортировки, который не рассматривался на уроках
 
+import random
+import statistics
+
+list = []
+
+for i in range(44):
+    list.append(random.randint(0, 50) * 2 + 1)
+print(list)
+static = statistics.median(list)
+listmin = []
+listmax = []
+
+for i in list:
+    if i < static:
+        listmin.append(i)
+    else:
+        listmax.append(i)
+
+print(listmin)
+print(listmax)
